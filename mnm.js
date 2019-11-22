5b2174bcd63f0d27aeff00466a182201cc928ce5
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-11-22 16:10:31 +0300

    Update mnm.js

--- mnm.js
+++ mnm.js
@@ -1,4 +1,4 @@
-$(`<a href="سخخى" target="_blank" style=" font-family:'El Messiri',sans-serif;
+$(`<a href="قريآ" target="_blank" style=" font-family:'El Messiri',sans-serif;
   width:107px;
   color:#ff0000;
   text-decoration:none;
@@ -12,35 +12,4 @@ $(`<a href="سخخى" target="_blank" style=" font-family:'El Messiri',sans-seri
   " class="border" ><i class="fa fa-android"></i> التطبيق</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
 $(".ae.label.label-primary.fa.fa-gear").text(' ضبط');
 $(".ae.fa.label.label-primary.fa-users").text('غرف');
-$('.nav-tabs').addClass('fl').css('width', '100%');
-$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="sico/z1ddjdmlvp10.png" width="70%" style="">`)
-$("div#tlogins button.btn.btn-primary").click(function() {
-    var b = setInterval(function() {
-        var c = $(".pmsgc").length;
-        0 < c ? ($(` </div><center>
-`).appendTo("#mic"), clearInterval(b)) : console.log(c)
-    }, 1)
-}), $("div#tlogins button.btn.btn-primary").click(function() {
-    setTimeout(function() {
-        refreshonline()
-    }, 0)
-}), setInterval(function() {
-    var a = $("#dpnl");
-    0 < a.length && a.remove()
-}, 0), $(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`), $('#d0').append(`<label title="\u0634\u0631\u062D \u0627\u0633\u062A\u062E\u062F\u0627\u0645 \u0627\u0644\u0634\u0627\u062A"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-volume-up">&nbsp;</label>`);
-$(`<div><marquee direction="right" id="as43"scrollamount="3"><font class="a"> </font>  |آهلا وسهلا بكم في عراق هوست● · نورت وطنك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات 
- </marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
-$(`<div><marquee direction="right" id="as433"scrollamount="3"><font class="a"> </font> 
- </marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
-var _0x9e44 = ["\x62\x6F\x64\x79", "\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65", "\x3C\x6C\x69\x6E\x6B\x20\x72\x65\x6C\x3D\x22\x73\x74\x79\x6C\x65\x73\x68\x65\x65\x74\x22\x20\x68\x72\x65\x66\x3D\x22\x68\x74\x74\x70\x73\x3A\x2F\x2F\x72\x61\x77\x63\x64\x6E\x2E\x67\x69\x74\x68\x61\x63\x6B\x2E\x63\x6F\x6D\x2F\x6A\x66\x61\x2D\x77\x2F\x66\x61\x68\x64\x2F\x39\x37\x62\x65\x62\x62\x38\x62\x35\x63\x31\x65\x66\x63\x65\x39\x30\x31\x63\x34\x39\x63\x37\x34\x30\x66\x65\x39\x30\x33\x64\x65\x32\x35\x64\x63\x30\x34\x35\x62\x2F\u0633\u064A\u0643\u0648\u0631\u062A\u064A\x2D\x33\x33\x2E\x63\x73\x73\x22\x3E\x3C\x2F\x73\x74\x79\x6C\x65\x3E"];
-$(_0x9e44[2])[_0x9e44[1]](_0x9e44[0]) $("div#tlogins button.btn.btn-primary").click(function() {
-    var c = setInterval(function() {
-        var a = $(".pmsgc").length;
-        0 < a ? ($(`<img style="height: 100%;
-						width:100%;
-						 margin-top: 2px;
-						pointer-events:none;
-						margin-bottom: 5px;
-						"src="soon"></div><center>`).appendTo("div#chats"), clearInterval(c)) : console.log(a)
-    }, 1)
-});
+$('.nav-tabs').addClass('fl').css('width','100%');
