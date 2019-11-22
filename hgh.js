fec8b291a7bf2c091c604cbe906a863c87522c9e
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-11-23 00:43:27 +0300

    Create hgh.js

---
+++ hgh.js
@@ -0,0 +1,261 @@
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw =$(".pmsgc").length;if(usmsgw > 0){$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:none;"><img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://iqr30.com/sico/z1c70vol2e10.gif);" class="fl fitimg hand u-pic"><div class="uzr fl" style="padding:0px;width:80%"> <div style="padding:0px;width:100%;" class="fl"><img class="fl u-ico" alt="" src="https://iqr30.com/dro3/z1ceuu4hc610.gif"> <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand"></span></div><br><div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl"><div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: 17px;" class=" u-msg   break  fl">  مرحباً بك يآ 🌹  <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1></i> فضلاً لا أمراً عدم ذكر مواقع مُماثلة لنا في الخاص ومن يقدم بلاغاً عن أشخاص أستدعوه لحضور شاتٍ آخر ،سيحصل على مائة لايك ،يرجى إثبات ذلك بصورة</div>`).appendTo('div#chats')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);
+})
+$(`<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;margin-top: -35px;width: 22%;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">القوانين</a>
+<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;width: 22%;margin-top: -8px;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">اختصارات</a>
+`).appendTo('div#l1');
+$(`<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;margin-top: -26px;width: 22%;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">ق.السوابر</a>
+<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;width: 22%;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">اشتراكات</a>
+`).appendTo('div#l3');
+
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw =$(".pmsgc").length;if(usmsgw > 0){$('.fl.mini.u-msg').css({'margin': '1px 0','background-color': '#E0E4EF',border: '1px solid #4682B4',paddingTop: '1px',borderRadius: '10px',color: '#4682B4'});
+}else{console.log(usmsgw)} }, 2000);
+})
+
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw =$(".pmsgc").length;if(usmsgw > 0){$(`<center></i><audio controls="" loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio>
+</div><center>`).appendTo('div#chats')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);
+})
+
+
+//var aaaaa = document.location.host = "www.w99w.net";
+var xharf = document.location.host.length;
+if(xharf=== 12 || xharf===8){
+    $(`<img style="width:100%;margin-top: 1px;float: left;margin-bottom: 4px;" src="https://www.w99w.net/sico/z1cj5btr2m10.jpg" class="fr">`).insertBefore('.nav-tabs');
+
+$("a.fa.fa-user-plus").text('عضوية جديدة');
+$(`<img tabindex="0"  class="al120 fl " style="margin-left: -4px; padding:5px; width:40px;margin-top: -4px;" src="https://iraqiachats.com/wp-content/uploads/2018/08/web_camera.png" data-original-title="" title=""><img tabindex="0" class="al121 fl " style="padding:5px;width: 50px;margin-top: -6px;margin-left: -17px;margin-right: -8px;" src="https://iraqiachats.com/wp-content/uploads/2018/08/psd-microphone-icon.png" data-original-title="" title="">`).insertAfter('img.fl.nosel.emobox');
+$(".al120").click(function(){
+    alert("عذرا لا تمتلك الصلاحية ل الدخول الى غرف محادثات الفيديو اطلب من المسؤل ادخالك الروم ");
+});
+$(".al121").click(function(){
+    alert("عذرا لا تمتلك صلاحيات استخدام المحادثات الصوتية ");
+});
+
+$(`<style>
+div#d2bc .uzr.fl.corner.borderg {
+    border-radius: 9px !important;
+    margin: 3px 1px -1px 2px !important;
+    border: 5px solid transparent;
+    padding: 15px;
+    -webkit-border-image: url(https://iraqiachats.com/wp-content/uploads/2018/08/border1.png) 30 round; /* Safari 3.1-5 */
+    -o-border-image: url(https://iraqiachats.com/wp-content/uploads/2018/08/border1.png) 30 round; /* Opera 11-12.1 */
+    border-image: url(https://iraqiachats.com/wp-content/uploads/2018/08/border1.png) 30 round;
+}
+@font-face{font-family:'jazeera';src: url('https://www.fontstatic.com/fonts/jazeera/jazeera.eot?#iefix');src: local('الجزيرة'), local('jazeera'),url('https://www.fontstatic.com/fonts/jazeera/jazeera.woff') format('woff');}
+@font-face{font-family:'jazeera-light';src: url('https://www.fontstatic.com/fonts/jazeera-light/jazeera-light.eot?#iefix');src: local('الجزيرة خفيف'), local('jazeera-light'),url('https://www.fontstatic.com/fonts/jazeera-light/jazeera-light.woff') format('woff');}
+label.fl.label.loginstat.label-success {
+    background-color: #4a97ec;
+}
+div#l1 {
+    border-bottom: 3px solid #4682B4;
+    margin-bottom: 2px;
+    padding: 8px 1px!important;
+    border-left: 1px dotted #4682B4;
+    border-right: 1px dotted #4682B4;
+}
+div#l3 {
+    border-bottom: 3px solid #4682B4;
+    margin-bottom: 2px;
+    padding: 8px 1px!important;
+    border-left: 1px dotted #4682B4;
+    border-right: 1px dotted #4682B4;
+}
+div#l2 {
+    border-bottom: 3px solid #4682B4;
+    margin-bottom: 2px;
+    padding: 8px 1px!important;
+    border-left: 1px dotted #4682B4;
+    border-right: 1px dotted #4682B4;
+}
+input#u2, input#pass1, input#u1, input#pass2, input#u3 {
+    text-align: center;
+    border: 1.5px solid #337ab7;
+    border-radius: 4px;
+    width: 50%!important;
+}
+div#l2 button.btn.btn-primary {
+    width: 85px;
+    position: inherit;
+    height: 26px;
+    margin-bottom: 4px;
+    padding: 0 0 3px;
+    font-family: 'jazeera',FontAwesome;
+}
+.checkbox {
+    margin-top: -25px!important;
+    margin-left: -83px!important;
+    position: fixed;
+    float: right;
+ /*   margin-right: 100px!important;*/
+}
+ul.nav.nav-tabs {
+    background-color: white!important;
+    border: none;
+    border-bottom: 3px solid #337ab7;
+    padding-bottom: 2px;
+}
+a.label.label-primary.fl img.fl {
+    display: none;
+}
+button.btn.fr.btn-success.fa.fa-refresh {
+    float: left;
+    margin-right: 5px!important;
+    margin-left: 5px!important;
+}
+a.label.label-primary.fl {
+    width: 68%!important;
+    border-radius: 5px!important;
+    margin: 5px -4px;
+    padding: 3px!important;
+    margin-top: 5px;
+    margin-bottom: 2px;
+    font-family: 'jazeera',FontAwesome;
+    font-size: 13px!important;
+    text-align: center!important;
+}
+.nav-tabs>li.active>a, .nav-tabs>li.active>a:focus, .nav-tabs>li.active>a:hover {
+    color: #337ab7;
+    background-color: #ffffff;
+    text-align: center;
+}
+.nav-tabs>li:hover {
+    background-color: #337ab7;
+}
+.nav-tabs>li>a:hover {
+    border-color: #ffffff #eee #ddd;
+    border: 2px solid;
+    background-color: #4a97ec;
+    color: #ffffff;
+}
+.nav-tabs>li {
+    background: #A6F0FF;
+    -webkit-animation: mymove 1s infinite; /* Chrome, Safari, Opera */
+    animation: mymove 6s infinite;
+    width: 33%;
+    border: 1px solid #a7a4a4;
+    margin-left: 1px;
+    border-radius: 6px 6px 0 0;
+    height: 40px!important;
+}
+/* Chrome, Safari, Opera */
+@-webkit-keyframes mymove {
+    from {background-color: #A6F0FF;}
+    to {background-color: white;}
+}
+/* Standard syntax */
+@keyframes mymove {
+    from {background-color: #A6F0FF;}
+    to {background-color: white;}
+}
+.nav-tabs>li>a {
+    color: #ffffff;
+    margin: auto auto;
+    border: 1px solid #0b3361;
+    text-align: center;
+    background-color: #337ab7;
+    width: 95%;
+    height: 90%!important;
+    margin-top: 2px;
+    padding: 5px 0px!important;
+    font-family: 'jazeera',FontAwesome;
+}
+button.btn.fr.btn-success.fa.fa-refresh {
+    padding: 0px;
+    border-radius: 15px;
+    background-image: linear-gradient(to bottom,#337ab7 0,#093e6b 100%);
+    border-color: #0b5ca2;
+}
+label.label.label-primary.mini.fl {
+    width: 34%!important;
+    padding: 1px;
+    border-radius: 0px 0px 0px 5px!important;
+    margin-top: -3px;
+    float: right;
+}
+.fr.borderg {
+    padding: 1px;
+    border-radius: 0px 0px 5px 0px!important;
+    margin-top: -3.50px;
+    background-color: #4682B4!important;
+    border: .1px solid #4682B4;
+    float: left;
+    color: #fff;
+    height:20.40px;
+}
+img.fr.co {
+    margin: 4px 6px;
+    border-radius: 5px!important;
+}
+img.fitimg.fl.u-pic {
+    border-radius: 6px;
+}
+/*input#u1, input#pass2, input#u3, input#pass1, input#u2 {
+    border-color: #c9c7c7;
+    float: right;
+    margin-top: 2px;
+    width: 51%;
+}*/
+body.bg,div#d2 {
+    background-color: #f1eff1!important;
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+    padding: 0px 3px 2px;
+    margin-right: 5px;
+}
+.fr.borderg, .fr.borderg a {
+    font-size: 12px !important;
+    padding: 0 5px!important;
+    color: #fff;
+    
+}
+label.fl.label.loginstat {
+    float: right!important;
+    border-radius: 5px!important;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 5px -4px;
+    padding: 1px 8px 4px!important;
+    margin-top: 5px;
+    font-family: 'jazeera',FontAwesome;
+    font-size: 13px !important;
+}
+div#d0 span {
+    background-color: red;
+}
+</style>`).insertBefore('body');
+$(".hand.nosel.fl.uzr.border").css({"margin":"0px 1px 2px 2px ","border-radius":"5px","border-color":"#d3d2d0"});
+}else{
+xharf = "www.w99w.net";
+}
+$(`<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;margin-top: -26px;width: 22%;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">التطبيق</a>
+<a href="#" class="btn btn-primary" style="padding: 1px 5px 3px;width: 22%;float: right;position: relative;margin-right: 1px;font-family: 'jazeera',FontAwesome;font-size: 13px!important;background-color: #ffffff!important;color: #337ab7;">الاشتراكات</a>
+`).appendTo('div#l2');
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+   $(`
+	<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+      <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(room.png);" class="fl fitimg hand u-pic    ">
+      <div class="uzr fl" style="padding:0px;width:80%">
+        <div style="padding:0px;width:100%;" class="fl">
+          <img class="fl u-ico" alt="" src="sico/z1cj4c8t8r10.png">
+          <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">ADMIN CHAT</span>
+        </div>
+        <br>
+        <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+		<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: right;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+      </div>
+    </div></div>
+	`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+var _0x70c2=["div#tlogins input#u1","insertBefore","<font style=\"width: 100%;display: block;font-size: 14px!important;font-family: 'jazeera-light',FontAwesome;margin-bottom: 2px;text-align: center;\">"];$(_0x70c2[2])[_0x70c2[1]](_0x70c2[0]);var _0x564a=["div#d2bc","insertBefore","\r\n"];$(_0x564a[2])[_0x564a[1]](_0x564a[0]);var _0xe4a3=["click","alcolor","addClass","asim","indexOf","text","filter","div#users .fl.mini.u-msg","on","label.ae.fa.label.label-primary.fa-user"],_0xdd61=[_0xe4a3[0],_0xe4a3[1],_0xe4a3[2],_0xe4a3[3],_0xe4a3[4],_0xe4a3[5],_0xe4a3[6],_0xe4a3[7],_0xe4a3[8],_0xe4a3[9]];$(_0xdd61[9])[_0xdd61[8]](_0xdd61[0],function(){$(_0xdd61[7])[_0xdd61[6]](function(){return-0<$(this)[_0xdd61[5]]()[_0xdd61[4]](_0xdd61[3])})[_0xdd61[2]](_0xdd61[1])});var _0xb650=["maxlength","12","attr","div#tlogins input"];$(_0xb650[3])[_0xb650[2]](_0xb650[0],_0xb650[1]);
+function logout(){setTimeout(function(){close(0)})}
