d38a0698f29279ee9aaa80335e257fbc0ff9eba0
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-11-22 20:58:52 +0300

    Create nb5.js

---
+++ nb5.js
@@ -0,0 +1,37 @@
+$('<img src="https://5.top4top.net/p_1415i3ued1.gif" class="fr" style="width:100%;margin-top: 0px;">\x0D\x0A<a id="des3" href="https://www.m3gic.com/" style="    text-align: right!important;transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:70%;margin:-172px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">شات عراق هوست جوال</a>\x0D\x0A <a id="des3" href="https://iraqechat.com/" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:24%;margin:-138px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">الإدآإره</a>\x0D\x0A <a id="des3" href="https://iraqechat.com/" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:27%;margin:-104px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">الأشتــراكات</a>\x0D\x0A <a id="des3" href="https://iraqechat.com/" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:30%;margin:-69px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">زخرفة أسماء</a>\x0D\x0A <a id="des3" href="https://www.1des.net/m3gicname.html" style="transform:matrix(1, 0, 0, 1, -3, 0);border-radius: 24px;width:33%;margin:-34px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">بنــرات أسمــاء</a>\x0D\x0A\x0D\x0A\x0D\x0A')['insertBefore']('.nav-tabs');
+$('.hand.nosel.fl.uzr.border')['css']({
+    'margin': '1px 0'
+    , borderRight: '1px solid #1a90a5'
+    , borderLeft: '1px solid #1a90a5'
+    , borderTop: '1px solid #4F565E'
+    , borderBottom: '1px solid #4F565E'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.uzr.fl.corner.borderg')['css']({
+    'margin': '0px 0'
+    , borderRight: '1px solid #1a90a5'
+    , borderLeft: '1px solid #1a90a5'
+    , borderTop: '.40px solid #a3a9b0'
+    , borderBottom: '.50px solid #a3a9b0'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.room.borderg.hand.nosel.fl')['css']({
+    'margin': '1px 0'
+    , borderRight: '1px solid #1a90a5'
+    , borderLeft: '1px solid #1a90a5'
+    , borderTop: '1px solid #4F565E'
+    , borderBottom: '1px solid #4F565E'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.corner.fa.fa-user.label.label-primary.fr.uc')['css']('border-radius', '5px');
+$('[data-target=#wall]')['attr']('title', '  ♥️ مســـاحه حــره لأبــدآعاتكــم ♥️           ');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('<link href="" rel="stylesheet">\x0D\x0A<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/nb4.css">\x0D\x0A<style>\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    font-size: 10px!important;\x0D\x0A    padding: 0 5px!important;\x0D\x0A}\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    border-radius: 3px!important;\x0D\x0A    background-color: #1c91a7!important;\x0D\x0A    background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);\x0D\x0A    border: black solid 0px;\x0D\x0A    color: #fff;\x0D\x0A}\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    margin-top: 5px;\x0D\x0A}\x0D\x0Aa.fa.fa-user {\x0D\x0A    color: rgb(255, 255, 255)!important;\x0D\x0A}\x0D\x0A.hmsg {background-color: rgb(235, 241, 255)!important;}\x0D\x0Aa.label.label-primary.fl {display: none}\x0D\x0A#des1 {margin-bottom: 2px;font-family: \'El Messiri\',FontAwesome!important;background-image: linear-gradient(90deg,#61adcc 0,#3390b3 10%,#1e6a87 51%,#3390b3 89%,#61adcc 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}\x0D\x0A\x0D\x0A</style>')['insertBefore']('body');
+$('.u-pic')['css']('border-radius', '3px');
+$('button.btn.btn-primary')['css']('border-radius', '3px');
+$('.label-primary')['css']('border-radius', '3px');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%')
