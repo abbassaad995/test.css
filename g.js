8cca862f31f7c58e5d6ceb98c01958ac79af3b05
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-11-22 16:21:21 +0300

    Create g.js

---
+++ g.js
@@ -0,0 +1,23 @@
+$(`<a href="https://play.google.com/store/apps/details?id=com.jfa.net" target="_blank" style=" font-family:'El Messiri',sans-serif;
+  width:107px;
+  color:#ff0000;
+  text-decoration:none;
+  margin-right:5px;
+  margin-left:2px;
+  text-align:center;
+  float:right;
+  border-radius:5px;
+  border-color:#F2F2F2;
+  padding:3px;
+  " class="border" ><i class="fa fa-android"></i> التطبيق</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط');
+$(".ae.fa.label.label-primary.fa-users").text('غرف');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="sico/z1ddjdmlvp10.png" width="70%" style="">`)
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` </div><center>
+`).appendTo("#mic"),clearInterval(b)):console.log(c)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var a=$("#dpnl");0<a.length&&a.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`),eval(function(b,f,a,g,h,i){if(h=function(a){return(a<f?"":h(parseInt(a/f)))+(35<(a%=f)?String.fromCharCode(a+29):a.toString(36))},!"".replace(/^/,String)){for(;a--;)i[h(a)]=g[a]||h(a);g=[function(a){return i[a]}],h=function(){return"\\w+"},a=1}for(;a--;)g[a]&&(b=b.replace(new RegExp("\\b"+h(a)+"\\b","g"),g[a]));return b}("$('#C').d(`<1 6=\"\\4\\e\\f \\2\\g\\5\\h\\i\\2\\j \\2\\k\\4\\2\\5\"n=\"#\"c=\"$('.p').o($(8).r('6'));s($(8),'a');u(v(){$('#3').w(0);},0);$('.x').y();\"7-z=\"A\"7-q=\"#3\"b=\"B 9 1 1-a 9-m-l\">&t;</1>`);",39,39,["","label","u0627","mic","u0634","u062A","title","data","this","fa","primary","class","onclick","append","u0631","u062D","u0633","u062E","u062F","u0645","u0644","up","volume","href","text","pnhead","target","attr","hl","nbsp","setTimeout","function","scrollTop","dpnl","show","toggle","tab","ae","d0"],0,{}))
+$(`<div><marquee direction="right" id="as43"scrollamount="3"><font class="a"> </font>  |آهلا وسهلا بكم في شات عراق هوست ● · نورت وطنك هنا نورتنا ·● مع تمنياتنا للجميع بأطيب الاوقات 
+ </marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<div><marquee direction="right" id="as433"scrollamount="3"><font class="a"> </font> 
+ </marquee><div>`).insertBefore('div#tlogins .lonline.light.break');
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('B d=["\\8\\j\\a\\k","\\l\\m\\5\\3\\b\\6\\C\\3\\9\\j\\b\\3","\\r\\e\\l\\m\\s\\t\\b\\3\\e\\u\\f\\5\\6\\k\\e\\3\\5\\c\\3\\3\\6\\f\\t\\c\\b\\3\\9\\u\\f\\c\\6\\6\\D\\5\\E\\7\\7\\b\\g\\v\\4\\a\\m\\n\\F\\l\\6\\c\\g\\4\\s\\n\\4\\j\\G\\7\\H\\9\\g\\w\\v\\7\\9\\g\\c\\a\\7\\h\\x\\8\\3\\8\\8\\I\\8\\o\\4\\y\\3\\9\\4\\3\\h\\i\\y\\4\\p\\h\\4\\x\\p\\i\\9\\3\\h\\i\\q\\a\\3\\J\\o\\a\\4\\i\\p\\o\\8\\7\\K\\z\\L\\M\\N\\O\\z\\w\\q\\q\\n\\4\\5\\5\\f\\A\\r\\7\\5\\6\\k\\e\\3\\A"];$(d[2])[d[1]](d[0])',51,51,'|||x65|x63|x73|x74|x2F|x62|x66|x64|x72|x68|_0x9e44|x6C|x22|x61|x39|x30|x6F|x79|x69|x6E|x2E|x35|x34|x33|x3C|x6B|x20|x3D|x77|x2D|x37|x31|u064A|x3E|var|x42|x70|x3A|x67|x6D|x6A|x38|x32|u0633|u0643|u0648|u0631|u062A'.split('|'),0,{})),eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$("2#8 9.3.3-b").d(4(){5 c=e(4(){5 a=$(".f").g;0<a?($(`<h i="j: 6%;\n						k:6%;\n						 7-l: m;\n						n-o:p;\n						7-q: r;\n						"s="t://u-v.w.x/y.z"></2><A>`).B("2#C"),D(c)):E.F(a)},1)});',42,42,'||div|btn|function|var|100|margin|tlogins|button||primary||click|setInterval|pmsgc|length|img|style|height|width|top|2px|pointer|events|none|bottom|5px|src|http|reem|design|000webhostapp|com|dees|gif|center|appendTo|chats|clearInterval|console|log'.split('|'),0,{}))
