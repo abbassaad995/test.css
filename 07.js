f7a17c1e3c50511d682548351263923d7f383e97
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-12-01 18:44:33 +0300

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,36 @@
+$('<a id="des3" href="http://iraqstar1.com/" style="transform:matrix(1, 0, 0, 1, 117, 62);border-radius: 3px;width:18%;margin:-138px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">\u0627\u0644\u0625\u062F\u0622\u0625\u0631\u0647</a>\x0D\x0A<a id="des3" href="http://iraqstar1.com/" style="transform:matrix(1, 0, 0, 1, 27, -7);border-radius: 3px;width:23%;margin:-69px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">\u0627\u0644\u0625\u0634\u062A\u0631\u0627\u0643\u0622\u0625\u062A</a>\x0D\x0A <a id="des3" href="http://www.chaatom.com/z5r.html" style="transform:matrix(1, 0, 0, 1, 180, 112);border-radius: 3px;width:18%;margin:-138px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">\u0632\u062E\u0631\u0641\u0629</a>\x0D\x0A <a id="des3" href="http://iraqstar1.com/" style="transform:matrix(1, 0, 0, 1, 90, 43);border-radius: 3px;width:23%;margin:-69px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">\u0631\u0645\u0632\u064A\u0627\u062A</a>\x0D\x0A <a id="des3" href="http://iraqstar1.com/" style="transform:matrix(1, 0, 0, 1, 0, 8);border-radius: 3px;width:23%;margin:-34px 2px 2px;" class="fl mini  fa   fa-\x0D\x0A btn btn-primary">\u0645\u0635\u0645\u0645 \u0627\u0644\u0623\u0633\u062A\u0627\u064A\u0644</a>\x0D\x0A \x0D\x0A \x0D\x0A  <form name="jump">\x0D\x0A<div class="MyColors">\x0D\x0A					<map id="ColorsMap" name="ColorsMap">\x0D\x0A					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\x0D\x0A					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\x0D\x0A					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\x0D\x0A					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\x0D\x0A					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\x0D\x0A					<area alt="" coords="122, 0, 147, 25" href="javascript:chooseStyle(\'Co6\',60)" shape="rect" title="6">\x0D\x0A					<area alt="" coords="92, 0, 117, 25" href="javascript:chooseStyle(\'Co7\',60)" shape="rect" title="7">\x0D\x0A					</map>\x0D\x0A			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>\x0D\x0A</form>\x0D\x0A\x0D\x0A')['insertBefore']('.nav-tabs');
+$('.hand.nosel.fl.uzr.border')['css']({
+    '\x6D\x61\x72\x67\x69\x6E': '1px 0',
+    borderRight: '1px solid #1a90a5',
+    borderLeft: '1px solid #1a90a5',
+    borderTop: '1px solid #4F565E',
+    borderBottom: '1px solid #4F565E',
+    paddingTop: '2px',
+    borderRadius: '5px'
+});
+$('.uzr.fl.corner.borderg')['css']({
+    '\x6D\x61\x72\x67\x69\x6E': '0px 0',
+    borderRight: '1px solid #1a90a5',
+    borderLeft: '1px solid #1a90a5',
+    borderTop: '.40px solid #c1202045',
+    borderBottom: '.50px solid #c1202045',
+    paddingTop: '2px',
+    borderRadius: '5px'
+});
+$('.room.borderg.hand.nosel.fl')['css']({
+    '\x6D\x61\x72\x67\x69\x6E': '1px 0',
+    borderRight: '1px solid #1a90a5',
+    borderLeft: '1px solid #1a90a5',
+    borderTop: '1px solid #4F565E',
+    borderBottom: '1px solid #4F565E',
+    paddingTop: '2px',
+    borderRadius: '5px'
+});
+$('.corner.fa.fa-user.label.label-primary.fr.uc')['css']('border-radius', '5px');
+$['getScript']('https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('a[href="https://jawalhost.com/"]')['removeAttr']('href');
+$('<link rel="stylesheet" type="text/css" href="">\x0D\x0A\x0D\x0A<link href="https://raw.githack.com/abbassaad995/test.css/master/03.css" rel="stylesheet" type="text/css" />\x0D\x0A<link href="https://raw.githack.com/abbassaad995/test.css/master/04.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\x0D\x0A<link href="https://raw.githack.com/abbassaad995/test.css/master/05.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\x0D\x0A<link href="https://raw.githack.com/abbassaad995/test.css/master/06.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\x0D\x0A<link href="https://raw.githack.com/abbassaad995/test.css/master/06.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\x0D\x0A<style>\x0D\x0A\x0D\x0A.MyColors {\x0D\x0A    position: absolute;\x0D\x0A    transform:matrix(1, 0, 0, 1, -87, 2);\x0D\x0A	margin-top: 2px;\x0D\x0A    width: 300px;\x0D\x0A    height: 40px;\x0D\x0A    float: left;\x0D\x0A    background: transparent url(https://www.chaatom.com/sico/z1dq9sh7kj10.png) no-repeat scroll right top;\x0D\x0A    text-align: left;\x0D\x0A}\x0D\x0A\x0D\x0Aa.label.label-primary.fl {display: none}\x0D\x0A\x0D\x0A#des0 {\x0D\x0A	transform:matrix(1, 0, 0, 1, 63, -153);\x0D\x0A    position: absolute;\x0D\x0A    width: 50%!important;\x0D\x0A    height: 18px;\x0D\x0A    border-radius: 25px!important;\x0D\x0A    left: -50px;\x0D\x0A    font-size: 14px!important;\x0D\x0A    color: #fff;\x0D\x0A    border-left: 3px solid #fff;\x0D\x0A    border-right: 3px solid #fff;\x0D\x0A	padding: 0px 0!important;\x0D\x0A}\x0D\x0A#des3 {\x0D\x0A    border: 0 solid #000;\x0D\x0A	}\x0D\x0A</style>')['insertBefore']('body');
+$('[data-target=#wall]')['attr']('title', '  \u2665\uFE0F \u0645\u0633\u0640\u0640\u0640\u0627\u062D\u0647 \u062D\u0640\u0640\u0631\u0647 \u0644\u0623\u0628\u0640\u0640\u062F\u0622\u0639\u0627\u062A\u0643\u0640\u0640\u0645 \u2665\uFE0F           ');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%')
