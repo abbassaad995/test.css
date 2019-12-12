cee0db796632367f414ec7c5f46b8ae623e0d500
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-12-13 01:14:55 +0300

    Create عرب شات للجوال.js

---
+++ عرب شات للجوال.js
@@ -0,0 +1,73 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(`<img src="https://1.top4top.net/p_1431me1js1.gif
+" class="fr" style="width:100%;margin-right: 2px;">
+<a id="des3" href="" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa  fa-facebook
+ btn btn-primary"> فيس بوك</a>
+<a id="des3" href="#" style="border-radius: 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+ btn btn-default"> ميز نفسك</a>
+<a id="des3" href="#" style="border-radius: 24px;width:27%;margin:2px;" class="fl mini  fa fa-graduation-cap
+btn btn-primary"> قوانين موقع</a>
+<a id="des3" href="#" style="border-radius: 24px;width:22%;margin:2px;" class="fl mini  fa fa-phone
+btn btn-default"> اتصل بنا</a>
+<div style="width:100%;"  id="" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">إدآرة ألمـؤقع </div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>   صـآإحب إلـمـوقـع / ♪ شاعر الاحزان </p> <i class="fa fa-"></i>  
+<a href="#" style="border-radius: 90px 90px 90px 90px;width:48%;margin:2px;" class="fl mini  fa   fa-heartbeat
+ btn btn-primary"> .. الــمـحـبـة.. </a>
+<a href="#" style="border-radius: 90px 90px 90px 90px;width:49%;margin:2px;p dir="ltr" align="center" class="fl mini  fa fa-star
+btn btn-primary"> ..عـراق..  </a>
+ <center></div></div></div>
+`).insertBefore('.nav-tabs');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$(".border.corner").css("border-radius","24px 24px 24px 24px");
+
+
+$(`<center><div><div width="75.5%" style="background-color:#404040; color: #FF0000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"  
+><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >اهلآ وسهلاً بكم في شات عراق  المحبة للجوال   </font></div></div><center>`).insertBefore('#d2');
+
+$
+$(`<div><marquee direction="right" width="100%" id="des1" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">🌸 ادارة الموقع ترحب بكم   🌸 اهلا وسهلا بكم في شات عراق  المحبة  للجوال </marquee></div>`).insertBefore('div#tlogins .lonline.light.break');
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera" />
+<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/عراق.css">
+<style>
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px  rgb(0, 95, 105), 0 0 6px rgb(0, 95, 105);border-radius: 4px 4px 4px 4px;;}
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #005F69;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;}
+</style>`).insertBefore('body');
+
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.border {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(0, 77, 102);border: 1px solid white;}
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(230, 145, 188);border-radius: 75px;}
+ul.nav.nav-tabs.fl li a {      color: #57779b;  box-shadow: inset 0 2px 2px rgb(165, 69, 116), 0 0 8px rgb(165, 69, 117);font-family: 'jazeera',FontAwesome;font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px solid rgb(209, 157, 183);width: 33.3%;}
+.label-primary, .btn-primary {border-radius: 14px 14px 14px 14px;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 2px 2px rgb(206, 176, 176), 0 0 8px rgb(165, 69, 117);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 40px 40px 40px 40px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 3px solid #6b173f;font-family: 'jazeera',FontAwesome;border-radius: 5px 5px 5px 0px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 80%;text-align: -webkit-center;background-color: #6b173f;color: white;border-radius:12px 12px 12px 12px;}
+.fr.borderg a {color: #6b173f;}
+.fr.borderg {color: #6b173f;}
+.border.corner, .border.corner {box-shadow: inset 0 1px 1px rgb(165, 69, 116), 0 0 8px rgb(165, 69, 117);border: 2px solid #6b173f;}
+.label-primary, .btn-primary {border: 1px solid #d89191;}
+.checkbox {color: #6b173f;}
+</style>`).insertBefore('body');
+
+$(`<img style="height:50px;width: 100%;margin-right: 5px;background-color: #DE9DA5;" src="https://6.top4top.net/p_144161g921.gif" border="0">`).insertBefore('div#d2bc');
+$("div#tlogins button.btn.btn-primary").click(function() {
+        var myVar = setInterval(function() {
+                var usmsgw = $(".pmsgc").length;
+                if (usmsgw > 0) {
+                        $(`<center></i><audio controls="" loop="" play=""><source src="https://iraqiachats.com/iqr30.mp3"></audio>
+</div></i> <iframe src="https://tunein.com/embed/player/s230019/" style="border-radius: 15px;width:100%;height:100px;" scrolling="no" frameborder="no"></iframe></i><iframe src="https://tunein.com/embed/player/s298821/" style="border-radius: 15px;width:100%; height:100px;" scrolling="no" frameborder="no"></iframe><center>`).appendTo('div#chats')
+                        clearInterval(myVar);
+                } else {
+                        console.log(usmsgw)
+                }
+        }, 2000);
+})
