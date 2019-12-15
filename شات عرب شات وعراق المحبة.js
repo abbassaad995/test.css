80f3090cb8c9e9a0baee53f9ca391b32333634de
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-12-15 23:38:16 +0300

    Create شات عرب شات وعراق المحبة.js

---
+++ شات عرب شات وعراق المحبة.js
@@ -0,0 +1,54 @@
+
+
+$(`<img src="https://2.top4top.net/p_14441icbv1.gif" class="fr" style="width:100%;margin-right: 1px;">
+
+<div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">    شات عراق  المحبه للجوال السعادة تجمعنا  ●اهلا وسهلا بكم في دردشة عراق المحبة  ● · لا تدع الدردشة تلهيك عن صلاتك ولا تستخدم الشات في ما لا يرضي الله  </marquee></div>
+<div id="asim10"><center><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
+<div id="asim10"><center><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">عراق</a><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">المحبة</a>
+<div id="asim10"><center><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">مدونه</a><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">عرب</a>
+<div id="asim10"><center><a id="asim33" href="iraqstar1.com" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">شات</a>
+<style>
+`).insertBefore('.nav-tabs');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+
+$(`<link href="https://fonts.googleapis.com/css؟family=Rancho&effect=shadow-multiple|3d-float
+">
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+
+$(` <center><div><marquee 
+direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >   <font color="#FFFFFF">✧     ● • .مبدعين الحائط •  </font>
+ </marquee></font></div><center>`).insertBefore('div#d2bc');
+
+$("[data-target=#wall]").attr('title',' متنفس الأقلام والقلوب  ')
+$(``).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(`<center><div width="99.5%" style="background-image:url();color: #828282;border-radius: 5px 5px 5px5px;border-bottom: 1px solid #70C4DE;border-top: 1px solid#828282;padding-bottom: 1px!important;"><font style="background-color: #FFFFFF;border-radius: 8px 8px 8px 8px;font-family: 'jazeera',FontAwesome;padding: 1px 5px 1px 5px;margin: 3px 5px 3px 5px;">اهلإ وسهلا بكم ، في شآت عراق  المحبة  للجوال</font></div><center>`).insertBefore('#d2');
+
+$("[data-target=#wall]").attr('title',' الحائط للأبداع والشعر والأستمتاع ')
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jfa-w.net/"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">  
+</style>`).insertBefore('body');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">  
+</style>`).insertBefore('body');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">  
+</style>`).insertBefore('body');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">  
+</style>`).insertBefore('body');
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://raw.githack.com/abbose/as.js/master/aw.css">
+<style>
+<style>`).insertBefore('body');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera" />
+<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/عراق.css">
+<style>
+.fitimg.fl.u-pic  {box-shadow: 0 1px 1px -1px  rgb(0, 95, 105), 0 0 6px rgb(0, 95, 105);border-radius: 4px 4px 4px 4px;;}
+textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #005F69;margin-top: 2px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 2px solid #adadad;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;}
+</style>`).insertBefore('body');
