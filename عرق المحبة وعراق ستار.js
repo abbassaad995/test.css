2da26e1cb0fe4b0fc1b07d9add7399bfd9e6d415
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-12-15 22:37:24 +0300

    Create عرق المحبة وعراق ستار.js

---
+++ عرق المحبة وعراق ستار.js
@@ -0,0 +1,42 @@
+
+
+$(`<img src="https://2.top4top.net/p_14441icbv1.gif" class="fr" style="width:100%;margin-right: 1px;">
+
+<div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">    شات عراق  المحبه للجوال السعادة تجمعنا  ●اهلا وسهلا بكم في دردشة عراق المحبة  ● · لا تدع الدردشة تلهيك عن صلاتك ولا تستخدم الشات في ما لا يرضي الله  </marquee></div>
+<div id="asim10"><center><a id="asim33" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="asim33" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
+<a id="asim33" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; " id="asim33" class="btn btn-primary btn-sm collapsed" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">أدارة ألموقع</div><div class="collapse" id="collapseExample" aria-expanded="false" style="height: 6px;"><center><div class="well"><p lass="bg-primary">  </p>اهلااً وسهلااً بكم في شات ودردشة   <p></p></p> عراق المحبة  للجوال , حيث المتعه والاثاره والتشويق  <p>
+</p> عـزيزي الزائر / العضو الرجاء تجنب الدخول بـ الاسماء  <p>
+</p> الغير لائقة . والصور الفاضحه والابتعاد عن السياسه   <p>
+</p> لـكي لايتم حظرك بشكل دائم من الدردشة  <p>
+</p> أدارة شات ودردشة عرق المحبة للجوال   <p>
+</p> عباس - حسين<p>
+</p> لشـكاوي او الاقتـراحات .. <p>
+<img src="https://1.top4top.net/p_12221hrpa1.png">قريبآ>₳ℬℬÕŠË</BR><img </BR>_________ </BR>
+<img src="https://2.top4top.net/p_12229hwgb1.png">abbose0>تيليكرام</BR><img </BR>_________ </BR>
+<img src="https://2.top4top.net/p_12229hwgb1.png"> حسين </BR><img </BR>_________ </BR>
+<img src="https://2.top4top.net/p_12229hwgb1.png"> كرو </BR><img </BR>_________ </BR>
+<link rel="stylesheet" href="https://raw.githack.com/abbose/as.js/master/001.css">
+<style>
+`).insertBefore('.nav-tabs');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+
+$(`<link href="https://fonts.googleapis.com/css؟family=Rancho&effect=shadow-multiple|3d-float
+">
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+
+$(` <center><div><marquee 
+direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >   <font color="#FFFFFF">✧     ● • .مبدعين الحائط •  </font>
+ </marquee></font></div><center>`).insertBefore('div#d2bc');
+
+$("[data-target=#wall]").attr('title',' متنفس الأقلام والقلوب  ')
+$(``).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(`<center><div width="99.5%" style="background-image:url();color: #828282;border-radius: 5px 5px 5px5px;border-bottom: 1px solid #70C4DE;border-top: 1px solid#828282;padding-bottom: 1px!important;"><font style="background-color: #FFFFFF;border-radius: 8px 8px 8px 8px;font-family: 'jazeera',FontAwesome;padding: 1px 5px 1px 5px;margin: 3px 5px 3px 5px;">اهلإ وسهلا بكم ، في شآت عراق  المحبة  للجوال</font></div><center>`).insertBefore('#d2');
+
+$("[data-target=#wall]").attr('title',' الحائط للأبداع والشعر والأستمتاع ')
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://jfa-w.net/"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">  
+</style>`).insertBefore('body');
