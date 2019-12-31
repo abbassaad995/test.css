3d4198a7166126b2e7101e2b1912bd0bb7631596
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2019-12-31 23:45:47 +0300

    Create 00100.js

---
+++ 00100.js
@@ -0,0 +1,23 @@
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://1.top4top.net/p_1460hmmdn1.png" > <center>
+<div id="asim1"><a id="asim2"target="_blank"type="a"href="rules.html"style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;"class="btn minix btn-primary">القوانين</a><a id="asim2"target="_blank"type="a"href="sub.html"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">الاشتراكات</a><a id="asim2"target="_blank"type="a"href="iraqstar1.com/"style="border-radius: 0%; border-right: 1px solid#f5f8fa;"class="btn minix btn-primary">نسيم بغداد</a><a id="asim2"target="_blank"type="a"href="iraqstar1.comstyle="border-radius: 10% 0 0 0; border-right: 1px solid#f5f8fa; border-left: 2px solid#a6a6a6;"class="btn minix btn-primary">أبراج اليوم</a><a id="asim10"target="_blank"type="a"href="contact.htm"style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa"class="btn minix btn-primary">اتصل بنا</a><a id="asim10"target="_blank"type="a"href="iraqstar1.com"style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">الراديو</a><a id="asim10"target="_blank"type="a"href="mobile.html "style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">تطبيق</a><a id="asim10"target="_blank"type="a"href="Shortcut.html"style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6!important; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;"class="btn minix btn-primary">اختصارات</a><center>`).insertBefore(".nav-tabs"), eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(`<a href="قريبآ" target="_blank" style=" font-family:'El Messiri',sans-serif;
+  width:107px;
+  color:#ff0000;
+  text-decoration:none;
+  margin-right:5px;
+  margin-left:2px;
+  text-align:center;
+  float:right;
+  border-radius:5px;
+  border-color:#F2F2F2;
+  padding:3px;
+  " class="border" ><i class="fa fa-android"></i> التطبيق</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط');
+$(".ae.fa.label.label-primary.fa-users").text('غرف');
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+$(".border.corner").css("border-radius","15px 3px 15px 15px")
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/po1.css">
+<style>
+<style>`).insertBefore('body');
