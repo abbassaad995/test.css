693ae0d5e4161de619e12cff62008eb9d4ad924d
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2020-01-04 12:25:21 +0300

    Create teest.js

---
+++ teest.js
@@ -0,0 +1,44 @@
+$(`<img src="https://6.top4top.net/p_1451t6qhe1.gif" class="fr" style="width:100%;margin-right: 1px;">
+<div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">  اهلا وسهلا بكم في شات نسيم بغداد للجوال ♥ ●</marquee></div>
+
+<div id="asim10"><a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف شاغره</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات</a><a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اشتراكات</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">زخرفة</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">ق.السوبر</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a></div>
+`).insertBefore(".nav-tabs"),
+$(".nav-tabs").addClass("fl").css("width","100%"),
+$(".tc span[class=\"fa fl\"]").addClass("fa-check");
+var nopm=!0,nonot=!0;
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),
+$(`<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/iraqstar1.css"></style>`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="75%" style="">`), 
+$(`<a href="" target="_blank" style="width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" >
+`).insertBefore('.nav-tabs');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.break.light').css({"background-image":"url()"});
+$(` <center><div><marquee 
+direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >"  الحائط نسيم بغداد لبداعاتكم ♥ ●·     <font color="#FFFFFF">  </font>
+ </marquee></font></div><center>`).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(`<center><div width="99.5%" style="background-image:url();color: #828282;border-radius: 5px 5px 5px5px;border-bottom: 1px solid #70C4DE;border-top: 1px solid#828282;padding-bottom: 1px!important;"><font style="background-color: #FFFFFF;border-radius: 8px 8px 8px 8px;font-family: 'jazeera',FontAwesome;padding: 1px 5px 1px 5px;margin: 3px 5px 3px 5px;">اهلإ وسهلا بكم  في شآت  نسيم بغداد للجوال</font></div><center>`).insertBefore('#d2');
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://chatnsh.com"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">
+</style>`).insertBefore('body');
+$(".label-primary")
+  eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(".ae.label.label-primary.fa.fa-gear").text('الحائط'); 
+$(".ae.fa.label.label-primary.fa-users").text('الاعدادات');
+$('.nav-tabs').addClass('fl').css('width','100%');
+if(location.protocol!='https:'){location.protocol='https:';}
+
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط');
+$(".ae.fa.label.label-primary.fa-users").text('غرف');
+$('.nav-tabs').addClass('fl').css('width','100%');
