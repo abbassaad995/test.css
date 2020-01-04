3e1dd74c2ea6286a210a50e32909525d7affae16
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2020-01-04 12:30:09 +0300

    Create scob.js

---
+++ scob.js
@@ -0,0 +1,65 @@
+$(`<img src="https://6.top4top.net/p_1456pfos81.png" class="fr" style="width:100%;margin-right: 1px;">
+<div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">  اهلا وسهلا بكم في شات نسيم بغداد للجوال ♥ ●</marquee></div>
+
+<div id="asim10"><a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف شاغره</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات</a><a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اشتراكات</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">زخرفة</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">ق.السوبر</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a></div>
+`).insertBefore(".nav-tabs"),
+$(".nav-tabs").addClass("fl").css("width","100%"),
+$(".tc span[class=\"fa fl\"]").addClass("fa-check");
+var nopm=!0,nonot=!0;
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),
+$(`<link rel="stylesheet" href="https://raw.githack.com/abbassaad995/test.css/master/iraqstar1.css"></style>`).insertBefore("body");
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="" width="75%" style="">`), 
+$(`<a href="" target="_blank" style="width:107px;color: #E8415D;text-decoration:none; margin-right: 5px;margin-left: 2px;text-align: center;float: right;border-radius: 5px;border-color: #F2F2F2;padding: 3px;" class="border" >
+`).insertBefore('.nav-tabs');
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(`<link href="https://fonts.googleapis.com/cssطںfamily=Rancho&effect=shadow-multiple|3d-float
+">
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$('.break.light').css({"background-image":"url()"});
+$(` <center><div><marquee 
+direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2" >"  الحائط لبداعاتكم ♥ ●·     <font color="#FFFFFF">  </font>
+ </marquee></font></div><center>`).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(``).insertBefore('div#d2bc');
+$(`<center><div width="99.5%" style="background-image:url();color: #828282;border-radius: 5px 5px 5px5px;border-bottom: 1px solid #70C4DE;border-top: 1px solid#828282;padding-bottom: 1px!important;"><font style="background-color: #FFFFFF;border-radius: 8px 8px 8px 8px;font-family: 'jazeera',FontAwesome;padding: 1px 5px 1px 5px;margin: 3px 5px 3px 5px;">اهلإ وسهلا بكم  في شآت عراق هوست للجوال</font></div><center>`).insertBefore('#d2');
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('a[href="https://chatnsh.com"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="jawalhost.css  ">
+</style>`).insertBefore('body');
+$(".label-primary")
+  eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(".ae.label.label-primary.fa.fa-gear").text('الحائط'); 
+$(".ae.fa.label.label-primary.fa-users").text('الاعدادات');
+$('.nav-tabs').addClass('fl').css('width','100%');
+if(location.protocol!='https:'){location.protocol='https:';}
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.border {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(0, 77, 102);border: 1px solid white;}
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(230, 145, 188);border-radius: 75px;}
+ul.nav.nav-tabs.fl li a {      color: #57779b;  box-shadow: inset 0 2px 2px rgb(165, 69, 116), 0 0 8px rgb(165, 69, 117);font-family: 'jazeera',FontAwesome;font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px solid rgb(209, 157, 183);width: 33.3%;}
+.label-primary, .btn-primary {border-radius: 14px 14px 14px 14px;}
+textarea.fl.filw.corner.tbox {    box-shadow:inset 0 2px 2px rgb(206, 176, 176), 0 0 8px rgb(165, 69, 117);border-radius: 7px;padding-left: 5px;}
+label.label.tc.border.btn.fl {border-radius: 40px 40px 40px 40px;}
+.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 3px solid #6b173f;font-family: 'jazeera',FontAwesome;border-radius: 5px 5px 5px 0px;}
+label.fl.nosel.label.pnhead {font-family: 'jazeera',FontAwesome;width: 80%;text-align: -webkit-center;background-color: #6b173f;color: white;border-radius:12px 12px 12px 12px;}
+.fr.borderg a {color: #6b173f;}
+.fr.borderg {color: #6b173f;}
+.border.corner, .border.corner {box-shadow: inset 0 1px 1px rgb(165, 69, 116), 0 0 8px rgb(165, 69, 117);border: 2px solid #6b173f;}
+.label-primary, .btn-primary {border: 1px solid #d89191;}
+.checkbox {color: #6b173f;}
+</style>`).insertBefore('body');
+function logout() {
+    setTimeout(function(){  send('logout', {}); close(500); }, 10000);
+}
+$('.nav-tabs').addClass('fl').css('width','100%');
