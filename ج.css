2b7ff0b5d3cbb2f1a84d988fc6539d569cc01eb5
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2020-02-04 20:45:55 +0300

    Create ج.css

---
+++ ج.css
@@ -0,0 +1,27 @@
+$('.nav-tabs').addClass('fl').css('width','100%');$
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">
+</style>`).insertBefore('span.fl.fa.fa-send');
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px;}
+ul.nav.nav-tabs.fl li a {      color: #A1A1A1;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
+.fr.co {
+    transform: rotate(60deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+div#users img.fl.ustat {
+    border-radius: 35px 35px 35px 35px;
+    margin: 1;
+    margin-right: -38.5px;
+    margin-top: -1px;
+    width: 39px!important;
+    height: 39px!important;
+}
+</style>`).insertBefore('body');
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","35px 0px 35px 0px")
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '1px solid #FFFFFF',borderLeft: '1px solid #FFFFFF',borderTop: '.1px solid #FFFFFF',borderBottom: '.1px solid #FFFFFF',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
