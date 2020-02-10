3d3dd6aeb7aca08a32255fdfd29f5d3c091d7f13
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2020-02-11 00:52:59 +0300

    Create 6677.js

---
+++ 6677.js
@@ -0,0 +1,76 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`
+<div class="rainbow" style="width: 100%"><img src="https://i.top4top.io/p_1481w7bzk1.gif" class="fl" style="width:100%;margin-top: 1px;background-color: white;border-radius: 2px;"></div>
+<marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;">
+	
+	
+	اهلا وسهلا بكم في شات  عراق هوست للجوال | اداره الموقع  عباس 
+
+		</marquee>
+
+`).appendTo('.rainbowo');
+
+
+
+
+
+$( "#room button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" );
+$("#room .border.corner").css("border-radius","24px 24px 24px 24px");
+$(`
+<style>
+#room .label-primary,#room .label-warning ,#settings .label-primary,#settings .label-warning ,.dpnl .label-primary,.dpnl .label-warning {
+    background-image: -webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,#4c4b4b 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important;
+    border-radius: 24px!important;
+    border-left: .5px solid#c27ba0!important;
+    border-right: .5px solid#c27ba0!important;
+    border-top: .5px solid#9d778b!important;
+    border-bottom: .5px solid#9d778b!important;
+    border: 1px solid rgb(81, 72, 72)!important;
+}
+.room.borderg.hand.nosel.fl img.fl.u-pic {
+    margin: 1px 0;
+    border-Right: 3px solid rgb(81, 72, 72);
+    border-Left: 3px solid rgb(81, 72, 72);
+    border-Top: 1px solid rgb(81, 72, 72);
+    border-bottom: 1px solid rgb(81, 72, 72);
+    padding-Top: 3px;
+    border-Radius: 5px;
+}
+#room .u-pic ,#users .u-pic ,.room .u-pic {
+    border-radius: 24px 24px 24px 24px!important;
+    border-bottom: 1px solid rgb(81, 72, 72)!important;
+}
+#users .hand.nosel.fl.uzr.border, #users .hand.nosel.fl.uzr.border {
+    margin: 1px 0;
+    border-Right: 3px solid #ff6094;
+    border-Left: 3px solid #ff6094;
+    border-Top: 1px solid #ff6094;
+    border-bottom: 1px solid rgb(81, 72, 72);
+    padding-Top: 3px;
+    border-Radius: 5px;
+}
+div#users img.fl.ustat {
+    border-radius: 3px 37px 37px 37px!important;
+    margin: 0;
+    margin-right: -38.5px;
+    margin-top: -1px;
+    width: 39px!important;
+    height: 39px!important;
+}
+div#users .hand.nosel.fl.uzr.border {
+    margin: 0px 1px -1px 2px!important;
+}
+#room .fitimg.fl.u-pic {box-shadow: 0 1px 1px -1px rgb(0, 95, 105), 0 0 6px rgb(0, 95, 105);border-radius: 4px 4px 4px 4px;;}
+#room textarea.fl.filw.corner.tbox {border-radius: 10px;padding-left: 5px;border: 2px solid #480032;margin-top: 2px;}
+#room .label.tc.border.btn.fl, #room .label.tc.border.btn.fl,#settings .label.tc.border.btn.fl, #settings .label.tc.border.btn.fl {border: 2px solid #480032;font-family: 'jazeera',FontAwesome;border-radius: 24px 24px 24px 24px;} #tlogins{ border-radius: 0 !important; } #room .lonline.light.break {background-color: #dedede !important;} 
+</style>`).insertBefore('body');
+$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● •( إضغط هنا واستمع للراديو)• ● &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>راديو عراق هوست</p><audio controls loop="" play=""><source src="https://cdn1.esm3.com/music/4115/esm3.com.49777.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$('.break.light').css({"background-image":"url(sico/1574013805877.jpg)"});
+$(`<img style="height:50px;width: 100%;margin-right: 5px;background-color: #DE9DA5;" src="https://a.top4top.io/p_1481oj29n1.gif" border="0">`).insertBefore('div#d2bc')
+$('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #480032',borderLeft: '3px solid #480032',borderTop: '1px solid #480032',borderBottom: '1px solid #480032',paddingTop: '3px',borderRadius: '5px'});
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '3px solid #480032',borderLeft: '3px solid #480032',borderTop: '.40px solid #480032',borderBottom: '.50px solid #480032',paddingTop: '3px',borderRadius: '5px'}); 
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #480032',borderLeft: '3px solid #480032',borderTop: '1px solid #480032',borderBottom: '1px solid #480032',paddingTop: '3px',borderRadius: '5px'});
+$(`<center><font size="4" color="#9C3356"> <marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;"> مرحباً بك ضيفنا الكريم في شات عراق هوست ، من أجل راحتك، شات عراق هوست يجمع بين الجميع، شكراً لك على تعاونك معنا، ونتمنى لك يوماً سعيداً 👋</marquee></font><center>`).insertBefore('#d2');
+$(`<button style="width:45px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-success"></button>
+`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+(document.getElementById("tbox").placeholder = "( آگتب رسآلتك لـ شآت عراق هوست)");
