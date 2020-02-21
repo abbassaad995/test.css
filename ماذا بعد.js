71b9fbe839168652a2b5731b7637a8a870dfff74
iraq.star.com <57870462+abbassaad995@users.noreply.github.com>
2020-02-21 15:04:06 +0300

    Create ماذا بعد.js

---
+++ ماذا بعد.js
@@ -0,0 +1,69 @@
+$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
+$(document).ready($("div.fr.borderg").css("display","none"));
+if(location.protocol!='https:'){location.protocol='https:';}
+$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">تنبية هام أعزائي الأعضاء والزوار نود أن ننوه أنه تم حظر جميع دول البروكسي وفي حاله ظهور تم حظرك نأمل ايقاف برنامج تغيير الايبي وإعاده الدخول للدردشة مرحباً بكم نتمنى لكم قضاء أجمل الأوقات
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$("<a href=' ' target='_blank' class='fr btn btn-danger fa fa-star'>.. آعــــــــفــ،ـيـنْــــــيُ ، ، -</a>").insertAfter('.loginstat');
+$(`<img src="https://abo-arish.net/sico/z1dhtv4a7q10.gif" class="fr" style="width:100%;margin-right: 2px;"><a href="https://iraqechat.com/" style="width:90px;margin:2px;" class="fl mini fa fa-phone btn btn-warning"> اتصل بنا</a>
+<a href="https://iraqechat.com/" style="width:85px;margin:2px;" class="fl mini fa fa-heartbeat
+btn btn-danger"> التطبيـق</a>
+<a href="https://abo-arish2majed.000webhostapp.com/wp-content/uploads/2019/07/majed2.html" style="width:87px;margin:2px;" class="fl mini fa fa-star
+btn btn-success"> للإشتراك</a>
+<a href="https://abo-arish2majed.000webhostapp.com/wp-content/uploads/2019/07/www.abo-arish.net_.majedindex-1.html" style="width:90px;margin:2px;" class="fl mini fa fa-balance
+btn btn-info">قوانين الموقع</a>
+<div style="border-radius: 2px 9px 9px 9px;width:100%;margin:2px;border: 2px solid #8F0000;"  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> ادارة الموقع</div><div class="collapse" id="collapseExample"><center><div class="well">
+<b>
+<font color="#FF0000"> اداره  الموقع </font> </BR>
+</BR>  .. حسين </BR>
+ </BR> صــــاحب الموقع </BR>
+<font color="#0000FF">.. عباس</font></BR>
+`).insertBefore('.nav-tabs');
+$("div#tlogins button.btn.btn-primary").click(function(){var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="https://abo-arish.net/sico/z1dg6ar5js10.gif">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎ .. ' صاحب الموقع ، ،</span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl"> مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+
+}else{console.log(usmsgw)} }, 2000);
+})
+$(`<center><div><div width="99.5%" style="background-image:url(https://mrkzgulfup.com/uploads/156437100088551.gif);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid red;border-top: 2px solid red;padding-bottom: 2px!important;" ><font style="background-color: #E5F0F5;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >مرحبآ بكم في شآت عراق هوست</font></div></div><center>`).insertBefore('#d2');$('.nav-tabs').addClass('fl').css('width','100%');$
+('a[href="https://mrkzgulfup.com/uploads/15643711334562.gif"]').removeAttr('href');
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">
+</style>`).insertBefore('span.fl.fa.fa-send');
+(document.getElementById("tbox").placeholder = " مـآ يلـفظ من قـول الا لـديه رقـيب عتيـد ");
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px;}
+ul.nav.nav-tabs.fl li a {      color: #A1A1A1;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
+.fr.co {
+    transform: rotate(60deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+div#users img.fl.ustat {
+    border-radius: 35px 35px 35px 35px;
+    margin: 1;
+    margin-right: -38.5px;
+    margin-top: -1px;
+    width: 39px!important;
+    height: 39px!important;
+}
+</style>`).insertBefore('body');
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","35px 0px 35px 0px")
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '1px solid #FFFFFF',borderLeft: '1px solid #FFFFFF',borderTop: '.1px solid #FFFFFF',borderBottom: '.1px solid #FFFFFF',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
